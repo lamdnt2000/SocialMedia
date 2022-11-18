5ea1150b9928aad1eb4d1f637b98d2bafb389757
lamdnt2000 <sasukeanos@gmail.com>
2022-11-18 14:18:04 +0700

    --Fix Signup User

--- DataAccess/Models/LoginUser/InsertUserDTO.cs
+++ DataAccess/Models/LoginUser/InsertUserDTO.cs
@@ -11,7 +11,9 @@ namespace DataAccess.Models.LoginUser
     {
 
         public string Username { get; set; }
+        [Required]
         public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
 
 
