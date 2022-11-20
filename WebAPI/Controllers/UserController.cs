63a4e00c73d7632d21aabeea32a922f507d9b691
lamdnt2000 <sasukeanos@gmail.com>
2022-11-20 14:35:59 +0700

    --Fix role google Login

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -88,7 +88,7 @@ namespace WebAPI.Controllers
 
                 var user = await _userService.GoogleSignIn(TokenId);
                 _authService.SetCurrentUser(user);
-                return JsonResponse(200, "Success", new { Token = _authService.CreateToken(), FirstName = user.Firstname, LastName = user.Lastname, Role = user.Role.Name });
+                return JsonResponse(200, "Success", new { Token = _authService.CreateToken(), FirstName = user.Firstname, LastName = user.Lastname, Role = "Member" });
 
             }
             catch (Exception e)
